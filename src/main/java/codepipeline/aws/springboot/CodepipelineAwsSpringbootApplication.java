541ccb16e4751534d295c6ee32a416735ce275a6
Arya Ansuman Priyadarshi <arya.priyadarshi@ymail.com>
2021-02-08 23:53:09 +1100

    comscan

--- src/main/java/codepipeline/aws/springboot/CodepipelineAwsSpringbootApplication.java
+++ src/main/java/codepipeline/aws/springboot/CodepipelineAwsSpringbootApplication.java
@@ -2,8 +2,10 @@ package codepipeline.aws.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("codepipeline.aws.springboot")
 public class CodepipelineAwsSpringbootApplication {
 
 	public static void main(String[] args) {
